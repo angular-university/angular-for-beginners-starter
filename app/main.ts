c89db93240697b9213a147158af297e594afeea1
JHades <jhades.dev@gmail.com>
2016-06-24 14:32:52 +0200

    initial commit

---
+++ app/main.ts
@@ -0,0 +1,15 @@
+
+import {Component} from "@angular/core";
+import {bootstrap} from "@angular/platform-browser-dynamic";
+
+@Component({
+    selector:'my-app',
+    template: `<h1>Hello World !</h1>`
+})
+export class HelloWorld {
+
+
+
+}
+
+bootstrap(HelloWorld);
